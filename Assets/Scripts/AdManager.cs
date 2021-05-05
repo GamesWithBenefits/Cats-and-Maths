95f7e7a594b81b49e42ec7bebaee4fde5810ed00
Azan <azanulhaque@gmail.com>
2021-05-05 15:14:13 +0530

    Ad Debug #6

--- Assets/Scripts/AdManager.cs
+++ Assets/Scripts/AdManager.cs
@@ -1,33 +1,21 @@
 ﻿using GoogleMobileAds.Api;
-using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 
 public class AdManager : MonoBehaviour
 {
     public static AdManager Instance;
     private BannerView _bannerView;
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-        }
-        else
-        {
-            Destroy(gameObject);
-            return;
-        }
-        MobileAds.Initialize(initStatus => { });
-    }
 
     private void Start()
     {
-        _bannerView = new BannerView("ca-app-pub-4174137669541969/8894010796", AdSize.Banner, AdPosition.Bottom);
+        MobileAds.Initialize(initStatus => { });
+        
         RequestBanner();
     }
 
     private void RequestBanner()
     {
+        _bannerView = new BannerView("ca-app-pub-4174137669541969/8894010796", AdSize.Banner, AdPosition.Bottom);
         AdRequest request = new AdRequest.Builder().Build();
         _bannerView.LoadAd(request);
         _bannerView.Show();
