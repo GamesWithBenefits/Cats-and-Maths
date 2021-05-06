a6a7ffa972bf07cb0d68fd05a012bb1b40325031
Azan <azanulhaque@gmail.com>
2021-05-06 11:55:30 +0530

    Ad Debug #7

--- Assets/Scripts/AdManager.cs
+++ Assets/Scripts/AdManager.cs
@@ -3,14 +3,12 @@ using UnityEngine;
 
 public class AdManager : MonoBehaviour
 {
-    public static AdManager Instance;
     private BannerView _bannerView;
 
     private void Start()
     {
-        MobileAds.Initialize(initStatus => { });
-        
-        RequestBanner();
+        MobileAds.Initialize(initStatus => { RequestBanner();});
+       
     }
 
     private void RequestBanner()
@@ -18,11 +16,6 @@ public class AdManager : MonoBehaviour
         _bannerView = new BannerView("ca-app-pub-4174137669541969/8894010796", AdSize.Banner, AdPosition.Bottom);
         AdRequest request = new AdRequest.Builder().Build();
         _bannerView.LoadAd(request);
-        _bannerView.Show();
     }
 
-    private void OnDestroy()
-    {
-        _bannerView?.Destroy();
-    }
 }
