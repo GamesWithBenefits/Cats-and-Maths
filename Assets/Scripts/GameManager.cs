017f001aac3e79756a54c84aa6a1e3bc97117b45
Azan <azanulhaque@gmail.com>
2021-05-15 20:34:52 +0530

    Ad Debug RT #5

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -128,10 +128,10 @@ public class GameManager : MonoBehaviour
 
     public async void Pause()
     {
-        AdsManager.Instance.ShowAds(2);
-        AdsManager.Instance.ShowAds(3);
         pausePanel.transform.GetChild(0).GetComponent<Text>().text = _score.ToString();
         pausePanel.SetActive(true);
+        AdsManager.Instance.ShowAds(2);
+        AdsManager.Instance.ShowAds(3);
         await Task.Delay(500);
         Time.timeScale = 0;
     }
